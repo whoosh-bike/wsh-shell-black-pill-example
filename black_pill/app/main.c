dcf4be9e296e2fdc90dc99ddde51d078e9af02a8
Katbert <sergey92shilin@gmail.com>
2025-11-11 10:55:04 +0300

    add main func

---
+++ black_pill/app/main.c
@@ -0,0 +1,54 @@
+#include "main.h"
+// #include "usb_device.h"
+// #include "usbd_cdc_if.h"
+
+#include "shell.h"
+
+int _read(int file, char* ptr, int len) {
+    // char c;
+    // while (CDC_GetRxBufferBytesAvailable_FS() == 0) {
+    // }
+
+    // if (CDC_ReadRxBuffer_FS((uint8_t*)&c, 1) == USB_CDC_RX_BUFFER_OK) {
+    //     *ptr++ = c;
+    //     return 1;
+    // }
+
+    return -1;
+}
+
+int _write(int file, char* ptr, int len) {
+    (void)file;
+    // for (int i = 0; i < len; i++) {
+    //     char c = (char)(*ptr++);
+    //     while (CDC_Transmit_FS((uint8_t*)&c, 1) == USBD_BUSY) {
+    //     }
+    // }
+
+    return len;
+}
+
+void SystemClock_Config(void) {
+}
+
+int main(void) {
+    HAL_Init();
+
+    assert_param(0);
+
+    while (1)
+        ;
+}
+
+void Error_Handler(void) {
+    __disable_irq();
+    while (1) {
+        __BKPT();
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {
+    Error_Handler();
+}
+#endif /* USE_FULL_ASSERT */
---
+++ black_pill/app/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f4xx_hal.h"
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Error_Handler(void);
+
+#endif /* __MAIN_H */
